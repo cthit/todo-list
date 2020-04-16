eae8c953f2119b4dd171bc01ff589ff7cfd4672b
David Möller <davidm@student.chalmers.se>
2020-04-16 18:20:38 +0200

    Created a getter for all todos

--- backend/main.go
+++ backend/main.go
@@ -7,13 +7,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//Observe: the variable names in the struct starts with capital letter
+//if it starts with a lower case letter, it will not be converted to json
+type Todo struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+var todos []Todo
+var count = 1
+
 func main() {
 	fmt.Println("Starting application")
+
+	todos = []Todo{{0, "Hello", false}}
+
 	router := gin.Default()
 
-	router.GET("/todos", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "Hello World")
-	})
+	router.GET("/todos", HandleGet)
 
 	router.Run(":8080")
+}
+
+func HandleGet(c *gin.Context) {
+	c.JSON(http.StatusOK, todos)
 }
