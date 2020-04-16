75eaa979f44c70dff91079dff3320146d15ad562
David Möller <davidm@student.chalmers.se>
2020-04-16 19:17:07 +0200

    Frontend fetches todos from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DigitProviders } from "@cthit/react-digit-components";
 import { BrowserRouter, Switch } from "react-router-dom";
 import { Route } from "react-router";
-import { One, Two, Three } from "./levels";
+import { One, Two, Three, Four } from "./levels";
 
 const Home = () => <div>Home</div>;
 
@@ -15,6 +15,7 @@ const App = () => (
                     <Route exact path="/level/1" component={One} />
                     <Route exact path="/level/2" component={Two} />
                     <Route exact path="/level/3" component={Three} />
+                    <Route exact path="/level/4" component={Four} />
                 </Switch>
             </BrowserRouter>
         </BrowserRouter>
---
+++ frontend/src/levels/4.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import {
+    DigitDesign,
+    DigitText,
+    DigitLayout,
+} from "@cthit/react-digit-components";
+import ClearIcon from "@material-ui/icons/Clear";
+import CheckIcon from "@material-ui/icons/Check";
+import Axios from "axios";
+
+const Four = () => {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        Axios.get("/todos")
+            .then(res => setTodos(res.data))
+            .catch(err => {
+                console.log("Could not fetch todos");
+                console.log(err);
+            });
+    }, []);
+
+    const toggleTodo = index => {
+        let temp = [...todos];
+        temp[index].done = !temp[index].done;
+        setTodos(temp);
+    };
+
+    return (
+        <>
+            <DigitText.Heading2 text="Todo List" />
+            {todos.map((e, index) => (
+                <DigitDesign.Card key={e.id} onClick={() => toggleTodo(index)}>
+                    <DigitDesign.CardBody>
+                        <DigitLayout.Row>
+                            {e.done ? (
+                                <CheckIcon style={{ color: "green" }} />
+                            ) : (
+                                <ClearIcon style={{ color: "red" }} />
+                            )}
+                            <DigitText.Heading6 text={e.title} />
+                        </DigitLayout.Row>
+                    </DigitDesign.CardBody>
+                </DigitDesign.Card>
+            ))}
+        </>
+    );
+};
+
+export default Four;
