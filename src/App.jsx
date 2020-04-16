0b6bbc2b953e66119875313bc36f0b0f3c981da9
David Möller <davidm@student.chalmers.se>
2020-04-16 17:38:10 +0200

    Created level 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DigitProviders } from "@cthit/react-digit-components";
 import { BrowserRouter, Switch } from "react-router-dom";
 import { Route } from "react-router";
-import { One, Two } from "./levels";
+import { One, Two, Three } from "./levels";
 
 const Home = () => <div>Home</div>;
 
@@ -14,6 +14,7 @@ const App = () => (
                     <Route exact path="/" component={Home} />
                     <Route exact path="/level/1" component={One} />
                     <Route exact path="/level/2" component={Two} />
+                    <Route exact path="/level/3" component={Three} />
                 </Switch>
             </BrowserRouter>
         </BrowserRouter>
---
+++ src/levels/3.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+    DigitDesign,
+    DigitText,
+    DigitLayout,
+} from "@cthit/react-digit-components";
+import { todos } from "./todos";
+import ClearIcon from "@material-ui/icons/Clear";
+import CheckIcon from "@material-ui/icons/Check";
+import { useState } from "react";
+
+const Three = () => {
+    const [todoss, setTodos] = useState(todos);
+
+    const toggleTodo = index => {
+        let temp = [...todoss];
+        temp[index].done = !temp[index].done;
+        setTodos(temp);
+    };
+
+    return (
+        <>
+            <DigitText.Heading2 text="Todo List" />
+            {todoss.map((e, index) => (
+                <DigitDesign.Card key={e.id} onClick={() => toggleTodo(index)}>
+                    <DigitDesign.CardBody>
+                        <DigitLayout.Row>
+                            {e.done ? (
+                                <CheckIcon style={{ color: "green" }} />
+                            ) : (
+                                <ClearIcon style={{ color: "red" }} />
+                            )}
+                            <DigitText.Heading6 text={e.title} />
+                        </DigitLayout.Row>
+                    </DigitDesign.CardBody>
+                </DigitDesign.Card>
+            ))}
+        </>
+    );
+};
+
+export default Three;
