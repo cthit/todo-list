89726623e8b916d7d06ebff211d49408695aa725
molleer <damoller99@hotmail.se>
2020-04-18 14:36:54 +0200

    Created add method in node backend

--- node_backend/main.js
+++ node_backend/main.js
@@ -1,6 +1,8 @@
-express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 app = express();
+app.use(bodyParser.json());
 
 var count = 0;
 const todos = [
@@ -13,6 +15,16 @@ const todos = [
 
 app.get("/todos", (req, res) => {
     res.json(todos);
+});
+
+app.post("/todos", (req, res) => {
+    var todo = {};
+    todo.title = req.body.title;
+    todo.done = req.body.done;
+    todo.id = count++;
+    todos.push(todo);
+
+    res.status(201).end();
 });
 
 app.listen(8080);
